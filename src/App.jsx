9fb6cc434a520b40df086572ab9708d49a12ca2d
MARTIN EITAN CYWINER <48313358@ort>
2025-04-28 15:29:55 -0300

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import Formulario from './components/formulario';
+import Formulario from './components/Cita';
 import Listado from './components/Listado';
 import './App.css';
 
 function App() {
   return (
-    <div className="app-container">
-      <Formulario/>
-      <Listado/>
+    <div className="container">
+      <h1>ADMINISTRADOR DE PACIENTES</h1>
+      <div className="row">
+        <div className="one-half column">
+          <h2>CREAR MI CITA</h2>
+          <Formulario/>
+        </div>
+        <div className="one-half column">
+          <h2>ADMINISTRA TUS CITAS</h2>
+          <Listado/>
+          <Listado/>
+          <Listado/>
+
+
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import Cita from './Cita';
+import React, { useState } from "react";
 import './Listado.css';
 
-function Listado() {
-  return (
-    <div className="listado-container">
-      <h2>Listado</h2>
-      <Cita />
-      <Cita />
-      <Cita />
-    </div>
-  );
+const TarjetaCita = (props) => {
+    const [visible, setVisible] = useState(true);
+
+    const handleEliminar = () => {
+        setVisible(false); 
+    }
+
+    if (!visible) return null;
+
+    return (
+        <div className="cita">
+            <p>Mascota: <span>{props.nombre}</span></p>
+            <p>Dueño: <span>{props.dueño}</span></p>
+            <p>Fecha: <span>{props.fecha}</span></p>
+            <p>Hora: <span>{props.hora}</span></p>
+            <p>Sintomas: <span>{props.sintomas}</span></p>
+            <button className="button eliminar u-full-width" onClick={handleEliminar}>
+                Eliminar ×
+            </button>
+        </div>
+    );
 }
 
-export default Listado;
+export default TarjetaCita;
